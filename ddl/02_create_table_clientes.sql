61976732fc964b448fc36c24239fcfde6493951e
lucaspajarito2023-debug <lucaspajarito2023@gmail.com>
2026-08-12 06:48:44 -0600

    feat(ddl): crear tabla clientes

--- ddl/02_create_table_clientes.sql
+++ ddl/02_create_table_clientes.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS clientes(
     id_cliente INT AUTO_INCREMENT PRIMARY KEY,
     nombre_completo VARCHAR(160) NOT NULL,
-    identificacion VARCHAR(15) NOT NULL, 
+    identificacion VARCHAR(15) NOT NULL,
     direccion VARCHAR(90) NOT NULL,
     telefono VARCHAR(9) NOT NULL,
     email VARCHAR(60) NOT NULL UNIQUE
-)Engine=innoDB;
+)Engine=innoDB; 
