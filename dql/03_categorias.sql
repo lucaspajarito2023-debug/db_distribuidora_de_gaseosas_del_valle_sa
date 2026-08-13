ee34ebb0a6e27b81ab97c331e08087cf86bbb0ef
lucaspajarito2023-debug <lucaspajarito2023@gmail.com>
2026-08-12 20:36:57 -0600

    feat(01_clientes): insertar datos categorias_productos

---
+++ dql/03_categorias.sql
@@ -0,0 +1,10 @@
+INSERT INTO categorias (nombre_categoria)
+ VALUES 
+ ('Gaseosas'), ('Jugos'), 
+ ('Agua Purificada'), ('Energizantes'), 
+ ('Té Frío'), ('Bebidas Deportivas'), 
+ ('Agua Saborizada'), ('Refrescos'), 
+ ('Bebidas Isotónicas'), ('Bebidas Carbonatadas'), 
+ ('Bebidas Light'), ('Bebidas Zero Azúcar'), 
+ ('Bebidas Premium'), ('Bebidas Familiares'), 
+ ('Bebidas Importadas');
